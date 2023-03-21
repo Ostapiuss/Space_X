13cb24c7f9990cc0f507527156a88352fec56091
ostap <ostapokolovych@gmail.com>
2023-03-21 13:40:08 +0200

    [FE] Initial Commit with configure project

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import '../../node_modules/modern-normalize/modern-normalize.css';
+
+export const App = () => {
+  return (
+    <div className="app-root">
+      React homework template
+    </div>
+  );
+};
