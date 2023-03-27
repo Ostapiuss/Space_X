81d4f82ec93a9000669564fe378371861d14444d
ostap <ostapokolovych@gmail.com>
2023-03-27 03:50:12 +0300

    [FE] Resolve deploy

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('space-x-root')).render(
-  <BrowserRouter basename="/">
+  <BrowserRouter basename="Space_X">
     <App />
   </BrowserRouter>
 );
